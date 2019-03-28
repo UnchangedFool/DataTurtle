a8ba364dd3d913408cb19fa71ce8a7d74ce55e44
UnchangedFool <serokira@web.de>
2019-03-28 20:07:48 +0100

    Serving first login page

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express = require("express");
+import path = require("path");
 
 const app: express.Application = express();
+
 const port = 3000;
 
-app.use(express.static("public"));
-app.get("/", ( req, res) => res.send("Hello World!"));
-app.get("/", ( req, res) => res.send("Hello World!"));
+app.use(express.static(".dist/public"));
+app.get("/", ( req, res) => {
+    res.sendFile(path.join(__dirname + "/../views/login.html"));
+});
 
 app.listen(port, () => console.log("Example app listening on port ${port}!"));
